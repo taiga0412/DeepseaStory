a715956bf416cff1317c84b84959d41ff0b1df1f
[taiga0412] <b2202410@photon.chitose.ac.jp>
2022-05-09 13:18:38 +0900

    init

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private string groundTag = "Ground";
+    private bool isGround = false;
+    private bool isGroundEnter, isGroundStay, isGroundExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsGround()
+    {
+        if (isGroundEnter || isGroundStay)
+        {
+            isGround = true;
+        }
+        else if (isGroundExit)
+        {
+            isGround = false;
+        }
+        isGroundEnter = false;
+        isGroundStay = false;
+        isGroundExit = false;
+        return isGround;
+    }
+
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == groundTag)
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == groundTag)
+        {
+            isGroundStay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == groundTag)
+        {
+            isGroundExit = true;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectCollison.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollison : MonoBehaviour
+{
+    public float boundHeight;
+    [HideInInspector]public bool playerStep;
+}
